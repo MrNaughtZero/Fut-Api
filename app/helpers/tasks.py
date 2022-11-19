535033c7bff780a7fcfd0235d97b0b085ca63c35
MrNaughtZero <neil.naught@icloud.com>
2022-11-19 18:48:18 +0000

    create new email tasks

--- app/helpers/tasks.py
+++ app/helpers/tasks.py
@@ -7,3 +7,17 @@ def exception_occurred(error):
         Emails().exception_occurred(error)
     except Exception as e:
         raise Exception(e)
+
+@celery.task
+def players_updated(amount_of_players):
+    try:
+        Emails().players_update(amount_of_players)
+    except Exception as e:
+        raise Exception(e)
+
+@celery.task
+def check_unknown_cards():
+    try:
+        Emails().unknown_cards()
+    except Exception as e:
+        raise Exception(e)
