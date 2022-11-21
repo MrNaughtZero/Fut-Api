4cd72cb11c4e5c7b1755c8b779776f30dfbda2bd
MrNaughtZero <neil.naught@icloud.com>
2022-11-21 21:10:34 +0000

    updated logger

--- app/helpers/logger.py
+++ app/helpers/logger.py
@@ -1,13 +1,2 @@
 import logging
-from os import environ
-
-if environ.get("development") == "0":
-    filename = "logs/app.log"
-else:
-    filename = "/var/www/app/logs/log.log"
-
-logging.basicConfig(
-    filename=filename,
-    level=logging.DEBUG, 
-    format='%(asctime)s %(levelname)s %(name)s : %(message)s'
-)
+logging.basicConfig(filename='logs/log.log',level=logging.DEBUG)
