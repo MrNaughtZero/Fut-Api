40eb70d3a95507519d14fc6369a00494320333c4
MrNaughtZero <neil.naught@icloud.com>
2022-11-21 21:27:05 +0000

    updated log location

--- app/helpers/logger.py
+++ app/helpers/logger.py
@@ -1,2 +1,9 @@
 import logging
+from os import environ
+
+if environ.get("development") == "0":
+    location = "/var/www/app/logs/log.log"
+else:
+    location = "logs/log.log"
+
 logging.basicConfig(filename='logs/log.log',level=logging.DEBUG)
